15967d285ab1232c11169d4abd4e32b2f93ff269
redhi <51018201+redhi@users.noreply.github.com>
2021-04-16 23:50:48 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6064_18453626(AC).py
@@ -0,0 +1,5 @@
+﻿a,b,c = input().split()
+d = int(a)
+e = int(b)
+f = int(c)
+print((d if d<e else e) if ((d if d<e else e)< f) else f)
---
+++ CodeUp/Python기초100제/6065_18453638(AC).py
@@ -0,0 +1,7 @@
+﻿a,b,c = input().split()
+if(int(a)%2 ==0):
+    print(a)
+if(int(b)%2 == 0):
+    print(b)
+if(int(c)%2 == 0):
+    print(c)
