d96f244c6244a6703e8c63708f6ea3dc95e5a141
redhi <51018201+redhi@users.noreply.github.com>
2021-04-17 23:54:22 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6066_18468473(AC).py
@@ -0,0 +1,13 @@
+﻿a,b,c = input().split()
+if int(a)%2==0 :
+  print("even")
+else :
+  print("odd") 
+if int(b)%2==0 :
+  print("even")
+else :
+  print("odd") 
+if int(c)%2==0 :
+  print("even")
+else :
+  print("odd")
