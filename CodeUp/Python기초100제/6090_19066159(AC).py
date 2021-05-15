4ebaca163c5edf8ba99c6c4b4e64ebd18668d9b0
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-16 02:16:57 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6090_19066159(AC).py
@@ -0,0 +1,6 @@
+﻿a,m,d,n = map(int, input().split())
+result = a
+for i in range(n-1):
+    result = result*m+d
+print(result)
+
---
+++ CodeUp/Python기초100제/6091_19066191(AC).py
@@ -0,0 +1,5 @@
+﻿a,b,c = map(int,input().split())
+day=1
+while(day%a!=0 or day%b!=0 or day%c!=0):
+    day += 1
+print(day)
