4f110f28cb174b66af60c4facd97827248da9a8e
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-15 21:55:07 +0900

    Add files via upload

---
+++ 6083_19062991(AC).py
@@ -0,0 +1,10 @@
+﻿a,b,c = map(int,input().split())
+count = 0
+
+for i in range(a):
+    for j in range(b):
+        for k in range(c):
+            print('{} {} {}'.format(i,j,k)) # print(i,j,k) 보다 실행시간 빠름
+            count += 1
+                
+print(count)
