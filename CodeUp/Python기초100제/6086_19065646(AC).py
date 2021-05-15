e3c6ebc20a6f002463bac2ac3af01e0b3abf0388
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-16 01:27:47 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6086_19065646(AC).py
@@ -0,0 +1,7 @@
+﻿num = int(input())
+i = 1
+sum = 0
+while(sum<num):
+    sum += i
+    i += 1
+print(sum)
---
+++ CodeUp/Python기초100제/6087_19065789(AC).py
@@ -0,0 +1,7 @@
+﻿n = int(input())
+
+for i in range(1,n+1):
+    if(i%3==0):
+        continue
+    print(i,end=" ") # end=" " 줄 바꿈하지 않고 출력
+    
