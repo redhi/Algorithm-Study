7e7001a503157bee5e80cd8670c0cbcf1ecf71aa
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-16 00:59:27 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6084_19065218(AC).py
@@ -0,0 +1,4 @@
+﻿h,b,c,s = map(int,input().split())
+result = h*b*c*s/8/1024/1024
+print(round(result,1),"MB") #round(n,1) 소수점 첫째자리까지 반올림
+
---
+++ CodeUp/Python기초100제/6085_19065513(AC).py
@@ -0,0 +1,3 @@
+﻿w,h,b = map(int, input().split())
+result = w*h*b/8/1024/1024
+print("{:.2f}".format(round(result,2)),"MB") # "{:.2f}.format() 소수점 고정
