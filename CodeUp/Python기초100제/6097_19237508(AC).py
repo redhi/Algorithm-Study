36845616136026eea2a571be181fcea14b50056e
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-21 23:57:08 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6097_19237508(AC).py
@@ -0,0 +1,17 @@
+﻿h, w = map(int, input().split()) 
+mylist = [[0]*w for _ in range(h)]
+num = int(input())
+for i in range(num):
+  l, d, x, y = map(int, input().split())
+  
+  for j in range(l):
+      if d == 0:
+          mylist[x-1][y+j-1] = 1 
+      else:
+          mylist[x+j-1][y-1] = 1 
+          
+for i in range(h):
+    for j in range(w):
+        print(mylist[i][j],end=' ')
+    print()
+  
