71e732a6822e043fcf669b3df983ebae1852e0d3
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-05-24 00:46:33 +0900

    Add files via upload

---
+++ CodeUp/Python기초100제/6098_19269322(AC).py
@@ -0,0 +1,22 @@
+﻿mylist = [list(map(int, input().split())) for _ in range(10)]
+num = 1
+j2 = 1   
+for i in range(1,10):
+    if(num==2):
+        break
+    for j in range(1,10):
+        if(mylist[i][j2]==0):
+            mylist[i][j2]=9
+            j2 += 1
+            continue
+        elif(mylist[i][j2]==1):
+            j2 -= 1
+            break
+        elif(mylist[i][j2]==2):
+            mylist[i][j2]=9
+            num = 2
+            break
+for i in range(10):
+    for j in range(10):
+        print(mylist[i][j],end=' ')
+    print()
