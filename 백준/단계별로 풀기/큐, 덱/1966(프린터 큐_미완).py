aef347dc2eee433085be11234eb003302ec754f9
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-06-05 02:19:40 +0900

    Update 1966(프린터 큐_미완).py

--- 백준/단계별로 풀기/큐, 덱/1966(프린터 큐_미완).py
+++ 백준/단계별로 풀기/큐, 덱/1966(프린터 큐_미완).py
@@ -1,28 +1,25 @@
 from sys import stdin
 
 num = stdin.readline() # 여러 줄의 입력을 받아야 할 떄 input보다 stdin이 더 효율적
-my_deque = list()
+
 
 for _ in range(int(num)):
     N,M = map(int,stdin.readline().split())
-    my_list = list(map(int, stdin.readline().split()))
+    my_list = list(map(int, stdin.readline().split())) # 덱으로 선언하면 map 사용 불가
     goal = list(range(len(my_list))) # 해당 인덱스를 관리하기 위해 리스트 생성
-    
     goal[M] = 'goal'
-    print(goal)
-
-    idx = 0
-    for i in my_list:
-        if i == max(my_list):
-            idx += 1
+  
+    idx = 0 # 나가는 순서
+    while True:
+        if my_list[0] == max(my_list):
+            idx += 1 # 빠져나갈 때만 +1
             if goal[0] == 'goal':
                 print(idx)
                 break
             else:
                 my_list.pop(0)
                 goal.pop(0)
-                print(my_list)
-                print(goal)
+                
         else:
-            my_list.append(my_list.pop(0))
+            my_list.append(my_list.pop(0)) # 해당 리스트내에 가장 큰 값아니면 뒤에 붙이기
             goal.append(goal.pop(0))
