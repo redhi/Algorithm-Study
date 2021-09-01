02a84cb2d2229d12768d2f7e5cc5ff7387ca33e8
Minji Kim <51018201+redhi@users.noreply.github.com>
2021-09-01 23:57:10 +0900

    Update 1932(정수 삼각형).py

--- 백준/단계별로 풀기/동적 계획법1/1932(정수 삼각형).py
+++ 백준/단계별로 풀기/동적 계획법1/1932(정수 삼각형).py
@@ -5,7 +5,9 @@ for i in range(N):
   mylist.append(list(map(int, input().split())))
 
 for i in range(1,N):
+  
     for j in range(len(mylist[i])):
+      
       if j == len(mylist[i])-1:
           mylist[i][j] = mylist[i][j] + mylist[i-1][j-1]
       elif j==0:
