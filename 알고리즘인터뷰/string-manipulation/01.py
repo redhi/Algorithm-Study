e4eea898088ff2ee19ebe5268fddeb8de4a5e34a
김민지 <redji81@naver.com>
2021-09-04 00:17:51 +0900

    valid-palindrome

--- 알고리즘인터뷰/string-manipulation/01.py
+++ 알고리즘인터뷰/string-manipulation/01.py
@@ -13,8 +13,25 @@ def check(in_str):
             count += 1
 
     if count == (len(in_str)//2):
-        return True
-    return False
+        return "true"
+    return "false"
 
 
 print(check(in_str))
+
+
+class Solution:
+    def isPalindrome(self, in_str: str) -> bool:
+        count = 0
+        #in_str = in_str.replace(" ", "")
+        in_str = ''.join(char for char in in_str if char.isalnum())  # 특수문자제거
+        in_str = in_str.lower()
+
+        for i in range(len(in_str)//2):
+            if in_str[i] == in_str[-(i+1)]:
+                #print(in_str[i], in_str[-(i+1)])
+                count += 1
+
+        if count == (len(in_str)//2):
+            return True
+        return False
