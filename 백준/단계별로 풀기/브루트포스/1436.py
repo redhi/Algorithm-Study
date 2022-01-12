c08f1da9b0945b435a54226271ed6cab89fbfd3c
redhi <redji81@naver.com>
2022-01-12 23:47:48 +0900

    백준 브루트포스 문제 풀이

---
+++ 백준/단계별로 풀기/브루트포스/1436.py
@@ -0,0 +1,16 @@
+"""
+영화감독 숌
+"""
+
+N = int(input())
+
+
+six = 666
+count = 0
+while True:
+    if "666" in str(six):
+        count += 1
+    if count == n:
+        print(six)
+        break
+    six += 1
