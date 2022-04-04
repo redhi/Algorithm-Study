3bfcd2839681cdcc84febc8b9ccc9ddd1d62d839
redhi <redji81@naver.com>
2022-04-05 00:58:52 +0900

    백준 문제 풀이

--- 코테준비/백준/2504.py
+++ 코테준비/백준/2504.py
@@ -2,38 +2,66 @@ def sol(s):
     right_dict = {")": "(", "]": "["}
     stack = []
     answer = []
+
     for i in range(len(s)):
         if stack:
             if right_dict.get(s[i]) == stack[-1][0]:
                 if s[i] == ")" and stack[-1][1] == i - 1:
-                    answer.append(2)
-                    answer.append("+")
+                    if answer[-1] == "(":
+                        answer.pop()
+                    answer.extend(["2", "+"])
                 if s[i] == ")" and stack[-1][1] != i - 1:
-                    num = answer.pop()
-                    answer.append(num * 2)
+                    node = answer[-1]
+                    if node == "+":
+                        answer.pop()
+                    answer.extend([")", "*2"])
                 if s[i] == "]" and stack[-1][1] == i - 1:
-                    answer.append(3)
-                    answer.append("+")
+                    if answer[-1] == "(":
+                        answer.pop()
+                    answer.extend(["3", "+"])
                 if s[i] == "]" and stack[-1][1] != i - 1:
-                    num = answer.pop()
-                    answer.append(num * 3)
+                    node = answer[-1]
+                    if node == "+":
+                        answer.pop()
+                    answer.extend([")", "*3"])
                 stack.pop()
             else:
+                if answer[-1] == "*2" or answer[-1] == "*3":
+                    answer.append("+")
+                answer.append("(")
                 stack.append([s[i], i])
         else:
-            print("끝")
             if i != 0:
                 answer.append("+")
+            answer.append("(")
             stack.append([s[i], i])
-        print(stack)
-        print(answer)
+        # print(stack)
+        # print(answer)
+
     if stack:
         return 0
-    a = 0
-    # for i in range(len(answer)):
+    if answer[-1] == "+":
+        answer = answer[:-1]
+    answer = "".join(answer)
+    # print(answer)
 
-    print(stack)
+    answer = eval(answer)
+    return answer
 
 
-s = "(()[[]])([])"
+s = input()
+# # ((2+3)*2+3)*2
+# s = "(()[[]])([])"
+# # s = "((()[])[])"
+# # s = "()[()]"
+# s = "((()[()]()[()])()(()[()]()[()])[])((()[()]()[()])()(()[()]()[()])[])"
+# # ((2+(2*3)+2+(3*2))*2+2+(2+(3*2)+2+(3*2))*2*2+3)*2
+# # s = "[()[()[()]]]"
+# # s = "(()[()]()[()])(()[()]()[()])"
+# # s = "[](()[()]()[()])(()[()]()[()])"
+# # s = "[](()[()]()[()])()(()[()]()[()])"
+# # s = "[](()[()]()[()])()(()[()]()[()])[]"
 print(sol(s))
+
+
+# https://www.acmicpc.net/board/view/62519
