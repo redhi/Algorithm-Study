e3b32fec56103f47cbddb110d9b4666ac88454cc
redhi <redji81@naver.com>
2022-04-28 21:46:25 +0900

    프로그래머스 문제 풀이

---
+++ 코테준비/프로그래머스/규칙찾기/예상대진표.py
@@ -0,0 +1,46 @@
+def check(a, b):
+    if a % 2 == 0:
+        a //= 2
+    else:
+        a += 1
+        a //= 2
+    if b % 2 == 0:
+        b //= 2
+    else:
+        b += 1
+        b //= 2
+    if abs(b - a) != 0:
+        return True
+    return False
+
+
+def solution(n, a, b):
+    answer = 0
+    while check(a, b):
+        if a % 2 == 0:
+            a //= 2
+        else:
+            a += 1
+            a //= 2
+        if b % 2 == 0:
+            b //= 2
+        else:
+            b += 1
+            b //= 2
+        answer += 1
+
+    if abs(a - b) == 1:
+        answer += 1
+
+    return answer
+
+
+# 4 5 6
+# 5
+
+
+# 6364 32
+n = 8
+a = 4
+b = 5
+print(solution(n, a, b))
