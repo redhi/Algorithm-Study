df76f8a65915df46a2291e451789ff1e20e59494
redhi <redji81@naver.com>
2022-06-26 23:45:42 +0900

    프로그래머스 문제 풀이

--- 코테준비/프로그래머스/단속카메라.py
+++ 코테준비/프로그래머스/단속카메라.py
@@ -1,32 +1,23 @@
 def solution(routes):
     answer = 0
-    # print(max(routes))
+
     mm = -300001
     mi = 300001
     for r in routes:
         m = max(r)
         n = min(r)
-        # print(m, n)
         mm = max(mm, m)
         mi = min(n, mi)
-    # -20 -3
-    # 23
-    # -3 -(-3)
-    # -20 -(-3)
 
-    # print(mm, mi)
     arr = [0 for _ in range(mm - mi)]
-    # print(len(arr))
     for r in routes:
         s = r[0]
         e = r[1]
         for i in range(s, e):
             arr[abs(mi - i)] += 1
             print(i, abs(mi - i))
-    print(arr)
-    answer = len(routes) - max(arr)
 
-    # arr[abs()]
+    answer = len(routes) - max(arr)
 
     return answer
 
