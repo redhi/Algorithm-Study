f24fd0d3cff991a0392b306074768fa25f1c880b
redhi <redji81@naver.com>
2022-07-03 23:30:50 +0900

    프로그래머스 문제 풀이

--- 코테준비/프로그래머스/문자열/뉴스클러스터링.py
+++ 코테준비/프로그래머스/문자열/뉴스클러스터링.py
@@ -1,44 +1,60 @@
-from collections import defaultdict
+"""
+프로그래머스
+"""
+import math
 
 
 def solution(str1, str2):
     answer = 0
-    set1,set2 = set(),set()
-    dict1, dict2 = defaultdict(int), defaultdict(int)
+    lowstr1 = str1.lower()
+    lowstr2 = str2.lower()
 
-    for i in range(len(str1)):
-        s = str1[i : i + 2]
-        if len(s) == 2 and s.isalpha():
-            set1.add(s.lower())
-            dict1[s.lower()] += 1
+    list_str1 = [
+        lowstr1[i : i + 2]
+        for i in range(0, len(lowstr1) - 1)
+        if lowstr1[i : i + 2].isalpha()
+    ]
+    list_str2 = [
+        lowstr2[i : i + 2]
+        for i in range(0, len(lowstr2) - 1)
+        if lowstr2[i : i + 2].isalpha()
+    ]
+    set1 = [[i] for i in list_str1]
+    set2 = [[i] for i in list_str2]
+    alen = len(set1) + len(set2)
 
-    for j in range(len(str2)):
-        s = str2[j : j + 2]
-        if len(s) == 2 and s.isalpha():
-            set2.add(s.lower())
-            dict2[s.lower()] += 1
+    for i in range(len(list_str1)):
+        set1[i].append(list_str1.count(list_str1[i]))
+    set1 = set(list(map(tuple, set1)))
 
-    uni = list(set1 & set2)
-    sum_set = list(set1 | set2)
+    for i in range(len(list_str2)):
+        set2[i].append(list_str2.count(list_str2[i]))
+    set2 = set(list(map(tuple, set2)))
+    # print(set1)
+    # print(set2)
 
-    u_sum = 0
-    for i in range(len(uni)):
-        u_sum += min(dict1[uni[i]], dict2[uni[i]])
-    s_sum = 0
-    for i in range(len(sum_set)):
-        s_sum += max(dict1[sum_set[i]], dict2[sum_set[i]])
+    count = 0
 
-    if s_sum == 0:
+    for i in set1:
+        s1 = i[0]
+        n1 = i[1]
+        for j in set2:
+            s2 = j[0]
+            n2 = j[1]
+            if s1 == s2:
+                n1 = min(n1, n2)
+                count += n1
+    rall = alen - count
+    if rall == 0:
         answer = 65536
-    else:
-        answer = int((u_sum / s_sum) * 65536)
+        return answer
+    answer = math.trunc((count / rall) * 65536)
+    # print(answer)
+
     return answer
 
 
-str1 = "FRANCE"
-str2 = "french"
-# str1 = "handshake"
-# str2 = "shake hands"
-# str1 = "aa1+aa2"
-# str2 = "AAAA12"
-print(solution(str1, str2))
+str1 = "E=M*C^2"
+str2 = "e=m*c^2"
+str3 = "shake hands"
+solution(str1, str2)
